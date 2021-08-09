d98e951470efbdd3a9879ae490f4dd688620d220
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-08-09 10:47:34 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -127,7 +127,7 @@ async def auto_filter(bot, update):
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
             results.append([
-            InlineKeyboardButton("📂 " + file_names,"🗂️ " + url=file_link),
+            InlineKeyboardButton("📂 " + file_names, url=file_link),
             InlineKeyboardButton(f_size, url=file_link)
         ])
                 
