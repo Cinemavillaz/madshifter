e1801ee62ce86ac318d7020654bd73df39018f3a
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-08-12 17:19:08 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -18,36 +18,9 @@ https://t.me/joinchat/n2_vPoGuwig3ZGY1</b>
 <b>Press /help to know about available commands🤪</b>"""    
     
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+<b>നോക്കണ്ടെടാ ഉണ്ണി ഇത് നീ ഉദ്ദേശിച്ചത് അല്ല 😹 !.</b>
             
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-<b><a href="https://t.me/cinehut">cinemahut</a></b>
+<b><a href="https://t.me/cv_group1">𝙲𝙸𝙽𝙴𝙼𝙰 𝚅𝙸𝙻𝙻𝙰</a></b>
 """
     
     ABOUT_TEXT = """
