55a3580d04a37c86ad9c837e21374477f7a8c60f
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-08-16 14:14:16 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -62,7 +62,7 @@ async def auto_filter(bot, update):
     if filters:
         results.append(
                 [
-                    InlineKeyboardButton("📌 JOIN OUR MAIN CHANNEL ", url="https://t.me/cv_updatez")
+                    InlineKeyboardButton("📌 JOIN OUR MAIN CHANNEL ", url="https://t.me/cinema_clubadmin1")
                 ]
             )
         for filter in filters: # iterating through each files
@@ -86,7 +86,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"📂 {file_size} - {file_name}"
+            button_text = f"📂 {file_size} {file_name}"
             
 
             if file_type == "video":
